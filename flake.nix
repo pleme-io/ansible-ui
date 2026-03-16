28fcb295077bd3f801e6210a9beffab7c8cdb9b8
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:08 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -3,18 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          nodejs_22
-          python3
-        ];
-      };
-    });
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "shell";
+      description = "Ansible UI - web interface for AWX, Automation Hub, and Event-Driven Ansible";
+    };
 }
